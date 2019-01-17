2ae9c14ab819e1697e72f8a3ad59923d464d366b
Prince Donkor <prince.donkor74@gmail.com>
2019-01-16 20:16:22 -0600

    spring init

---
+++ db-service/src/main/java/com/princedonkor/stock/dbservice/DbServiceApplication.java
@@ -0,0 +1,14 @@
+package com.princedonkor.stock.dbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServiceApplication.class, args);
+	}
+
+}
+
